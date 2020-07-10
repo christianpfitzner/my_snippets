ac2abdbf65e98fcc0dae189b929c97d756ef2cc6
Christian Pfitzner <>
2020-07-10 14:27:40 +0200

    -- added tiny dialog gui for terminal

--- send_to_vs_code.sh
+++ send_to_vs_code.sh
@@ -7,6 +7,13 @@ echo "-------------------------------------------------------"
 
 vs_code_snippets_dir="$HOME/.config/Code/User/snippets/"
 
+vs_code_snippets_dir=`dialog --inputbox "VS Code snippet Directory" 0 0 "${vs_code_snippets_dir}" \
+  3>&1 1>&2 2>&3`
+# Dialog-Bildschirm löschen
+dialog --clear
+# Bildschirm löschen
+clear
+
 echo " Directory for the snippets is ${vs_code_snippets_dir}" 
 
 target="${vs_code_snippets_dir}latex.json"
