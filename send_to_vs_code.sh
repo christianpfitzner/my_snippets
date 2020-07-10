2477a4c2a8d4567b01320d67650f7482ca254994
Christian Pfitzner <>
2020-07-10 14:55:32 +0200

    -- modifiing latex snippets to have dummy names for each part

--- send_to_vs_code.sh
+++ send_to_vs_code.sh
@@ -11,8 +11,6 @@
 
 
 
-
-
 echo "-------------------------------------------------------"
 echo " Setting up the vs code snippets"
 echo "-------------------------------------------------------"
@@ -22,9 +20,7 @@ vs_code_snippets_dir="$HOME/.config/Code/User/snippets/"
 
 vs_code_snippets_dir=`dialog --inputbox "VS Code snippet Directory" 0 0 "${vs_code_snippets_dir}" \
   3>&1 1>&2 2>&3`
-# Dialog-Bildschirm löschen
 dialog --clear
-# Bildschirm löschen
 clear
 
 echo " Directory for the snippets is ${vs_code_snippets_dir}" 
